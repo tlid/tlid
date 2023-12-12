31d0853ad71fa7d777c7a818c6f0591ed26c0170
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-11 21:47:38 -0500

    chg:noarg-display-minutes

--- src/python/release-build.sh
+++ src/python/release-build.sh
@@ -1,4 +1,5 @@
 rm -rf dist build
 python3 setup.py sdist bdist_wheel && \
 	twine upload dist/*
-
+git push --tags &> /dev/null
+git push  &> /dev/null
--- src/python/tlid/tlidcli.py
+++ src/python/tlid/tlidcli.py
@@ -3,39 +3,52 @@
 
 import argparse
 import sys
-from . import (get_minutes, get_hour, get_seconds, get_milliseconds,
-                  get_month, get_year, get_day)
+from . import (
+    get_minutes,
+    get_hour,
+    get_seconds,
+    get_milliseconds,
+    get_month,
+    get_year,
+    get_day,
+)
+
 
 def main():
-        
-  # Create the parser
-  parser = argparse.ArgumentParser(description='TLID CLI - Generate time-based identifiers.')
-  
-  # Add arguments
-  parser.add_argument('unit', nargs='?', default='minutes',
-                      choices=['h', 's', 'ms', 'm', 'M', 'y', 'd'],
-                      help='Select the unit of time for TLID generation (default: minutes).')
-  
-  # Parse the arguments
-  args = parser.parse_args()
-  
-  # Get the TLID based on the selected unit
-  if args.unit == 'h':
-      print(get_hour())
-  elif args.unit == 's':
-      print(get_seconds())
-  elif args.unit == 'ms':
-      print(get_milliseconds())
-  elif args.unit == 'm' or args.unit == 'M':
-      print(get_month())
-  elif args.unit == 'y':
-      print(get_year())
-  elif args.unit == 'd':
-      print(get_day())
-  else:
-      print(get_minutes())
-      
-      
+    # Create the parser
+    parser = argparse.ArgumentParser(
+        description="TLID CLI - Generate time-based identifiers."
+    )
+
+    # Add arguments
+    parser.add_argument(
+        "unit",
+        nargs="?",
+        default="minutes",
+        choices=["h", "s", "ms", "m", "M", "y", "d", "minutes"],
+        help="Select the unit of time for TLID generation (default: minutes).",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Get the TLID based on the selected unit
+    if args.unit == "h":
+        print(get_hour())
+    elif args.unit == "s":
+        print(get_seconds())
+    elif args.unit == "ms":
+        print(get_milliseconds())
+    elif args.unit == "m" or args.unit == "M":
+        print(get_month())
+    elif args.unit == "y":
+        print(get_year())
+    elif args.unit == "d":
+        print(get_day())
+    elif args.unit == "minutes":
+        print(get_minutes())
+
+
 # This is the standard boilerplate that calls the main() function.
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
