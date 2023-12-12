955a1e79228f4281390a958372071d8df7d7eba0
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-11 21:44:27 -0500

    fix

--- src/python/tlid/tlidcli.py
+++ src/python/tlid/tlidcli.py
@@ -6,29 +6,36 @@ import sys
 from . import (get_minutes, get_hour, get_seconds, get_milliseconds,
                   get_month, get_year, get_day)
 
-# Create the parser
-parser = argparse.ArgumentParser(description='TLID CLI - Generate time-based identifiers.')
-
-# Add arguments
-parser.add_argument('unit', nargs='?', default='minutes',
-                    choices=['h', 's', 'ms', 'm', 'M', 'y', 'd'],
-                    help='Select the unit of time for TLID generation (default: minutes).')
-
-# Parse the arguments
-args = parser.parse_args()
-
-# Get the TLID based on the selected unit
-if args.unit == 'h':
-    print(get_hour())
-elif args.unit == 's':
-    print(get_seconds())
-elif args.unit == 'ms':
-    print(get_milliseconds())
-elif args.unit == 'm' or args.unit == 'M':
-    print(get_month())
-elif args.unit == 'y':
-    print(get_year())
-elif args.unit == 'd':
-    print(get_day())
-else:
-    print(get_minutes())
+def main():
+        
+  # Create the parser
+  parser = argparse.ArgumentParser(description='TLID CLI - Generate time-based identifiers.')
+  
+  # Add arguments
+  parser.add_argument('unit', nargs='?', default='minutes',
+                      choices=['h', 's', 'ms', 'm', 'M', 'y', 'd'],
+                      help='Select the unit of time for TLID generation (default: minutes).')
+  
+  # Parse the arguments
+  args = parser.parse_args()
+  
+  # Get the TLID based on the selected unit
+  if args.unit == 'h':
+      print(get_hour())
+  elif args.unit == 's':
+      print(get_seconds())
+  elif args.unit == 'ms':
+      print(get_milliseconds())
+  elif args.unit == 'm' or args.unit == 'M':
+      print(get_month())
+  elif args.unit == 'y':
+      print(get_year())
+  elif args.unit == 'd':
+      print(get_day())
+  else:
+      print(get_minutes())
+      
+      
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
